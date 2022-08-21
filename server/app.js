bcdd3a1df04e9aee3d9136678474e99cd7ebb936
Aditya <ranjan.aditya2901@gmail.com>
2022-08-21 16:34:46 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 
 dotenv.config({path:"./config.env"});
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(require("./route/auth"));
 if(process.env.NODE_ENV == "production"){
